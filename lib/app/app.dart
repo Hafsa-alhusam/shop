ed1c1a655cd2f656e8a24be691f62249153a1977
Hafsa-alhusam <hafsaalhusam793@gmail.com>
2024-03-06 16:42:39 +0300

    sort files

---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../ui/screen/splash.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Shop',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Splash(),
+    );
+  }
+}
